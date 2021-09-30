e234411809a5412541abe88196f1a834e4b6aaf0
Kyeongrok Kim <oceanfog1@gmail.com>
2021-09-30 16:16:18 +0900

    first commit

---
+++ minikube_install.sh
@@ -0,0 +1,10 @@
+sudo apt install virtualbox virtualbox-ext-pack -y
+
+curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 \
+  && chmod +x minikube
+sudo mkdir -p /usr/local/bin/
+sudo install minikube /usr/local/bin/
+
+sudo apt-get install -y conntrack
+
+sudo minikube start --vm-driver=none

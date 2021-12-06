0b842b6788564fc34e3225063b44a718cd42f740
root <root@ip-172-31-63-61.ap-northeast-2.compute.internal>
2021-12-06 12:46:04 +0000

    순서 변경 ohmybash, glances

--- utils.sh
+++ utils.sh
@@ -1,3 +1,3 @@
-sh -c "$(curl -fsSL https://raw.github.com/ohmybash/oh-my-bash/master/tools/install.sh)"
 apt update
 apt install glances -y
+sh -c "$(curl -fsSL https://raw.github.com/ohmybash/oh-my-bash/master/tools/install.sh)"

49932c01e3ea318b8ba36a1cb695cca252bd6943
Kyeongrok Kim <oceanfog1@gmail.com>
2022-02-14 13:44:14 +0900

    Update utils.sh

--- utils.sh
+++ utils.sh
@@ -2,4 +2,4 @@ apt update
 apt install glances -y
 bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh)"
 echo "alias k='kubectl'" >> .bashrc
-echo ". .bashrc"
+. .bashrc

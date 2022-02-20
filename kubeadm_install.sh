743ccf4b55ffef379a430f770df93addff259d0b
Ubuntu <ubuntu@ip-172-31-44-222.ap-northeast-2.compute.internal>
2022-02-20 06:04:40 +0000

    kubeadm

--- kubeadm_install.sh
+++ kubeadm_install.sh
@@ -1,7 +1,9 @@
 sudo apt-get update
 sudo apt-get install -y apt-transport-https ca-certificates curl
+# Add a GPG key for the Packages
 sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
 echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 sudo apt-get update
+# Install kubelet, kubeadm, kubectl
 sudo apt-get install -y kubelet kubeadm kubectl
 sudo apt-mark hold kubelet kubeadm kubectl

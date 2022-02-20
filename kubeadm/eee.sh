1a2ae271b27599ac43f73d1b428b62d21d0f62f3
root <root@ip-172-31-33-159.ap-northeast-2.compute.internal>
2022-02-20 07:33:34 +0000

    delete dir kubeadm

--- kubeadm/eee.sh
+++
@@ -1,7 +0,0 @@
-sudo apt-get update
-sudo apt-get install -y apt-transport-https ca-certificates curl
-sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
-echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
-sudo apt-get update
-sudo apt-get install -y kubelet=1.21.0-00 kubeadm=1.21.0-00 kubectl=1.21.0-00
-

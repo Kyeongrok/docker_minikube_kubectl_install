219de3eda4ba74bc1d681a5eb324466e3199398c
Kyeongrok Kim <oceanfog1@gmail.com>
2022-03-04 09:57:39 +0900

    Update kubeadm_install.sh

--- kubeadm_install.sh
+++ kubeadm_install.sh
@@ -16,11 +16,14 @@ cat <<EOF | sudo tee /etc/docker/daemon.json
 }
 EOF
 
+# Install Docker
+sudo apt-get update
+sudo apt-get install -y docker.io
+
 sudo systemctl enable docker
 sudo systemctl daemon-reload
 sudo systemctl restart docker
 
-sudo apt-get update
 sudo apt-get install -y apt-transport-https ca-certificates curl
 # Add a GPG key for the Packages
 sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg

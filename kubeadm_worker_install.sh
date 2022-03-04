93764069f50dc5ad62feca7dca2b74c2e44e08aa
Kyeongrok Kim <oceanfog1@gmail.com>
2022-03-04 14:51:50 +0900

    Update kubeadm_worker_install.sh

--- kubeadm_worker_install.sh
+++ kubeadm_worker_install.sh
@@ -4,6 +4,17 @@ sudo swapoff -a
 # hostname to 'node01'
 hostnamectl set-hostname 'node01'
 
+sudo mkdir /etc/docker
+cat <<EOF | sudo tee /etc/docker/daemon.json
+{
+  "exec-opts": ["native.cgroupdriver=systemd"],
+  "log-driver": "json-file",
+  "log-opts": {
+    "max-size": "100m"
+  },
+  "storage-driver": "overlay2"
+}
+EOF
 
 # Install Docker
 sudo apt-get install -y docker.io

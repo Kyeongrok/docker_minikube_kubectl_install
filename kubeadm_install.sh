f4b73eb256586a79d00fb2797d259b1a43282e96
Kyeongrok Kim <oceanfog1@gmail.com>
2022-03-08 13:32:40 +0900

    Update kubeadm_install.sh

--- kubeadm_install.sh
+++ kubeadm_install.sh
@@ -16,6 +16,11 @@ cat <<EOF | sudo tee /etc/docker/daemon.json
 }
 EOF
 
+cat <<EOF >  /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables = 1
+EOF
+
 # Install Docker
 sudo apt-get update
 sudo apt-get install -y docker.io

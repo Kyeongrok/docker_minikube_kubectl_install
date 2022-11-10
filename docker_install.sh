0835008fb1fe7b9b14dd85657b6965c9a6d8dd19
Kyeongrok Kim <oceanfog1@gmail.com>
2022-11-10 13:31:56 +0900

    Update docker_install.sh

--- docker_install.sh
+++ docker_install.sh
@@ -6,7 +6,7 @@ sudo apt-get install \
     gnupg-agent \
     software-properties-common -y
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository \
+sudo add-apt-repository --yes \
    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
    $(lsb_release -cs) \
    stable"
